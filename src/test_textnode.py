78e52bce34cf21a1ddec0f0141bc6eeaf305e97a
Dan <doliveri575@gmail.com>
2024-11-04 18:58:16 -0500

    Add tests to test_textnode.py

--- src/test_textnode.py
+++ src/test_textnode.py
@@ -9,6 +9,15 @@ class TestTextNode(unittest.TestCase):
         node2 = TextNode("This is a text node", TextType.BOLD)
         self.assertEqual(node, node2)
 
+    def test_url(self):
+        node = TextNode("This is a text node", TextType.BOLD, "https://www.testurl.tst")
+        self.assertIsNot(node.url, None)
+
+    def test_diff(self):
+        node = TextNode("This is a text node", TextType.ITALIC)
+        node2 = TextNode("This is a text node", TextType.BOLD)
+        self.assertNotEqual(node, node2)
+
 
 if __name__ == "__main__":
     unittest.main()
