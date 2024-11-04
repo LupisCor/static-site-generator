5b3b935676cf75ad38bbac142877f71f47d77d26
Dan <doliveri575@gmail.com>
2024-11-04 18:37:29 -0500

    Create .gitignore and textnode.py

--- src/main.py
+++ src/main.py
@@ -1,1 +1,9 @@
-# hello world
+from textnode import TextNode, TextType
+
+def main():
+    #print("hello world")
+    test = TextNode("this is a test", TextType.BOLD, "https://www.test.tst")
+    print(test)
+
+
+main()
