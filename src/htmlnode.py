e6b4a0689bcf005d0b3c8888a979126cd343214a
Dan <doliveri575@gmail.com>
2024-11-05 18:05:40 -0500

    Add classes LeafNode and ParentNode to htmlnode.py, create tests for these classes

--- src/htmlnode.py
+++ src/htmlnode.py
@@ -2,7 +2,7 @@ class HTMLNode:
     def __init__(self, tag=None, value=None, children=None, props=None):
         self.tag = tag # string
         self.value = value # string
-        self.childen = children # list
+        self.children = children # list
         self.props = props # dict
     
     def to_html(self):
@@ -18,4 +18,32 @@ class HTMLNode:
         return output
 
     def __repr__(self):
-        return f"HTMLNode({self.tag}, {self.value}, {self.childen}, {self.props})"
+        return f"HTMLNode({self.tag}, {self.value}, children: {self.children}, {self.props})"
+
+class LeafNode(HTMLNode):
+    def __init__(self, tag, value, props=None):
+        super().__init__(tag, value, None, props)
+
+    def to_html(self):
+        if self.value is None:
+            raise ValueError("All leaf nodes must have a value.")
+        if self.tag is None:
+            return self.value
+        return f"<{self.tag}{self.props_to_html()}>{self.value}</{self.tag}>"
+    
+    def __repr__(self):
+        return f"LeafNode({self.tag}, {self.value}, {self.props})"
+    
+class ParentNode(HTMLNode):
+    def __init__(self, tag, children, props=None):
+        super().__init__(tag, None, children, props)
+    
+    def to_html(self):
+        if self.tag is None:
+            raise ValueError("All ParentNodes must have a tag.")
+        if self.children is None:
+            raise ValueError("All ParentNodes must have at least one ChildNode")
+        children_html = ""
+        for child in self.children:
+            children_html += child.to_html()
+        return f"<{self.tag}{self.props_to_html()}>{children_html}</{self.tag}>"
