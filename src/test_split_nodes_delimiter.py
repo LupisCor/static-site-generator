588d6e1051e3c51a088239f46a74904f61697fa8
Dan <doliveri575@gmail.com>
2025-03-06 18:00:55 -0500

    Empty Commit

---
+++ src/test_split_nodes_delimiter.py
@@ -0,0 +1,18 @@
+import unittest
+from split_nodes_delimiter import(split_nodes_delimiter)
+from textnode import TextNode, TextType
+
+class TestSplitNodeDelimiter(unittest.TestCase):
+    def test_delim_bold(self):
+        node = TextNode("This is text with a **bolded** word", TextType.TEXT)
+        new_nodes = split_nodes_delimiter([node], "**", TextType.BOLD)
+        self.assertListEqual(
+            [
+                TextNode("This is text with a ", TextType.TEXT),
+                TextNode("bolded", TextType.BOLD),
+                TextNode(" word", TextType.TEXT),
+            ],
+            new_nodes,
+        )
+if __name__ == "__main__":
+    unittest.main()
