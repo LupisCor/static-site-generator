a5df323573850e0ad90e533d72dbafab08deb25c
Dan <doliveri575@gmail.com>
2025-03-17 19:30:34 -0400

    Add block_to_block_type function and tests

---
+++ src/split_blocks.py
@@ -0,0 +1,46 @@
+from enum import Enum
+import re
+
+class BlockType(Enum):
+    PARAGRAPH = "paragraph"
+    HEADING = "heading"
+    CODE = "code"
+    QUOTE = "quote"
+    UNORDERED_LIST = "unordered_list"
+    ORDERD_LIST = "ordered_list"
+
+def markdown_to_blocks(markdown):
+    new_list = []
+    block_list = markdown.split("\n\n")
+    for block in block_list:
+        if block == "":
+            continue
+        block = block.strip()
+        new_list.append(block)
+    return new_list
+    
+def block_to_block_type(block):
+    lines = block.split("\n")
+
+    if block.startswith(("# ", "## ", "### ", "#### ", "##### ", "###### ")):
+        return BlockType.HEADING
+    if len(lines) > 1 and lines[0].startswith("```") and lines[-1].startswith("```"):
+        return BlockType.CODE
+    if block.startswith(">"):
+        for line in lines:
+            if not line.startswith(">"):
+                return BlockType.PARAGRAPH
+        return BlockType.QUOTE
+    if block.startswith("- "):
+        for line in lines:
+            if not line.startswith("- "):
+                return BlockType.PARAGRAPH
+        return BlockType.UNORDERED_LIST
+    if block.startswith("1. "):
+        i = 1
+        for line in lines:
+            if not line.startswith(f"{i}. "):
+                return BlockType.PARAGRAPH
+            i += 1
+        return BlockType.ORDERD_LIST
+    return BlockType.PARAGRAPH
